dd45e87b1e9d3bc6998b8b64ee1c4eb275185b8f
Rebecca Ehlers <rebecca.ehlers@og1o.de>
2026-02-09 10:48:31 +0100

    new learning - javascript

---
+++ js-fundamentals/lessons/lesson1.js
@@ -0,0 +1,27 @@
+console.log("Hello World");
+
+var firstName = "John";
+let lastName = "Smith";
+
+console.log(firstName);
+
+
+var age, dateOfBirth, sex;
+age = 30;
+sex = "male";
+dateOfBirth = "01/01/1990";
+
+console.log(age);   
+console.log(dateOfBirth);
+console.log(sex);
+sex = "female";
+console.log(sex);
+
+const occupation = "Software Engineer";
+console.log(occupation);
+
+var middleName = "William";
+var ageOfBrother = 25;
+var isMarried = false;
+var yearsInMarriage = null;
+var numberOfCarsOwned = undefined;
