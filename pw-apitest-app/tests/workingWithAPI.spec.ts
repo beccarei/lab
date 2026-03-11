3879f8155c1a7442faf3f71b663b8dbea65dcbb0
Rebecca Ehlers <rebecca.ehlers@og1o.de>
2026-03-11 12:53:32 +0100

    course: continue from 0 to hero APIs

---
+++ pw-apitest-app/tests/workingWithAPI.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+import tags from '../test-data/tags.json';
+
+test.beforeEach(async ({ page }) => {
+  // mocking the API response for tags
+  await page.route('*/**/api/tags', async route => {
+    await route.fulfill({
+      body: JSON.stringify(tags)
+    })
+  })
+  await page.waitForTimeout(500)
+  await page.goto('https://conduit.bondaracademy.com/');
+});
+
+test('has title', async ({ page }) => {
+  await expect(page.locator('.navbar-brand')).toHaveText('conduit');
+});
