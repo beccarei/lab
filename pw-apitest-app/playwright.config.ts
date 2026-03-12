3cc9b8a9bc31cea729f99749d9c48cf94ac1698f
Rebecca Ehlers <rebecca.ehlers@og1o.de>
2026-03-12 11:13:29 +0100

    continued with course

--- pw-apitest-app/playwright.config.ts
+++ pw-apitest-app/playwright.config.ts
@@ -30,13 +30,19 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    ignoreHTTPSErrors: true,
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        ...devices['Desktop Chrome'],
+        launchOptions: {
+          args: ['--ignore-certificate-errors']
+        }
+      },
     },
 
     {
--- pw-apitest-app/tests/workingWithAPI.spec.ts
+++ pw-apitest-app/tests/workingWithAPI.spec.ts
@@ -1,17 +1,42 @@
-import { test, expect } from '@playwright/test';
+import { expect, test, request } from '@playwright/test';
 import tags from '../test-data/tags.json';
 
 test.beforeEach(async ({ page }) => {
+
   // mocking the API response for tags
   await page.route('*/**/api/tags', async route => {
     await route.fulfill({
       body: JSON.stringify(tags)
     })
   })
-  await page.waitForTimeout(500)
+
+  await page.waitForTimeout(20000)
   await page.goto('https://conduit.bondaracademy.com/');
 });
 
 test('has title', async ({ page }) => {
+  await page.route('https://conduit-api.bondaracademy.com/api/articles*', async route => {
+    const response = await route.fetch()
+    const responseBody = await response.json()
+    responseBody.articles[0].title = 'My MOCK Title'
+    responseBody.articles[0].description = 'My MOCK Description'
+    await route.fulfill({
+      body: JSON.stringify(responseBody)
+    })
+  })
+  await page.getByText('Global Feed').click();
   await expect(page.locator('.navbar-brand')).toHaveText('conduit');
+  await expect(page.locator('app-article-list h1').first()).toContainText('My MOCK Title');
+  await expect(page.locator('app-article-list p').first()).toContainText('My MOCK Description');
 });
+
+test('delete article', async ({ request }) => {
+
+  const response = await request.post('https://conduit-api.bondaracademy.com/api/users/login', {
+    
+    data: { "user": { "email": "ehlers.becca@gmail.com", "password": "XFeFAK@TaTYgi8Y" } }
+  })
+
+  const responseBody = await response.json()
+  console.log(responseBody.user.token)
+})
